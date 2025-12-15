104c947f48ae443eb0cb11c60bdb9c7a8e14ab04
Ankit-nehra <ankitnehra7000@gmail.com>
2025-12-15 21:27:55 +0530

    Update StudentDashboard.jsx

--- frontend/src/pages/StudentDashboard.jsx
+++ frontend/src/pages/StudentDashboard.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import GroupChat from "../Component/GroupChat";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://studentdriverbackend-3.onrender.com");
 
 export default function StudentDashboard() {
   const location = useLocation();
@@ -93,3 +93,4 @@ export default function StudentDashboard() {
     </div>
   );
 }
+
