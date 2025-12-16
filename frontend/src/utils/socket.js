928b385b9d7c91491c8712cbc9218f585eff25ab
Ankit-nehra <ankitnehra7000@gmail.com>
2025-12-16 12:51:33 +0530

    Update socket.js

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://studentdriverbackend-3.onrender.com", {
   autoConnect: false,
 });
 
 export default socket;
+
